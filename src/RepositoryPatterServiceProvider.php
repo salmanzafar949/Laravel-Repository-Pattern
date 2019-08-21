4b3121a2332ba81b001d914e305dbc1dcc1369c4
Salman Zafar <salman.zafar@innowi.com>
2019-08-21 16:14:57 +0500

    removed config

--- src/RepositoryPatterServiceProvider.php
+++ src/RepositoryPatterServiceProvider.php
@@ -14,10 +14,14 @@ class RepositoryPatterServiceProvider extends ServiceProvider
      */
     public function register()
     {
+//        $this->mergeConfigFrom(__DIR__.'/config/RepositoryConfig.php','RepositoryPattern');
+
         $this->loadViewsFrom(__DIR__.'/resources/stubs', 'RepositoryPattern');
 
         $this->publishes([
             __DIR__.'/resources/stubs' => resource_path('vendor/salmanzafar/stubs'),
+//            __DIR__.'/config/RepositoryConfig.php' => config_path('RepositoryConfig.php'),
+
         ]);
     }
 
